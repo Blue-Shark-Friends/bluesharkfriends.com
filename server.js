b0f787f7bb2714c7e0018eac5dc52bca7c9c2e51
called2voyage <thesboys@gmail.com>
2024-03-14 16:19:10 -0600

    Pull common head structure out into an EJS Partial

--- server.js
+++ server.js
@@ -8,8 +8,9 @@ app.use(express.static(__dirname + '/'));
 // use res.render to load up an ejs view file
 
 // index page
+let index_data = require('./views/data/index.json')
 app.get('/', function(req, res) {
-  res.render('pages/index');
+  res.render('pages/index', {index_data: index_data});
 });
 
 // about page
@@ -19,13 +20,15 @@ app.get('/about', function(req, res) {
 });
 
 // solutions page
+let solutions_data = require('./views/data/solutions.json')
 app.get('/solutions', function(req, res) {
-    res.render('pages/solutions');
+    res.render('pages/solutions', {solutions_data: solutions_data});
 });
 
 // contact page
+let contact_data = require('./views/data/contact.json')
 app.get('/contact', function(req, res) {
-    res.render('pages/contact');
+    res.render('pages/contact', {contact_data: contact_data});
 });
 
 app.listen(8080);
