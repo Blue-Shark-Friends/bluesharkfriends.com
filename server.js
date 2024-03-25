cd396e3eb4c0acec865b0a12204ddc7fd233a187
called2voyage <thesboys@gmail.com>
2024-03-25 11:58:47 -0600

    Create branding data json and implement in all pages and partials

--- server.js
+++ server.js
@@ -7,34 +7,36 @@ app.use(express.static(__dirname + '/'));
 
 // use res.render to load up an ejs view file
 
+let branding_data = require('./views/data/_branding.json')
+
 // index page
 let index_data = require('./views/data/index.json')
 app.get('/', function(req, res) {
-  res.render('pages/index', {index_data: index_data});
+  res.render('pages/index', {index_data: index_data, branding_data: branding_data});
 });
 
 // about page
 let about_data = require('./views/data/about.json')
 app.get('/about', function(req, res) {
-  res.render('pages/about', {about_data: about_data});
+  res.render('pages/about', {about_data: about_data, branding_data: branding_data});
 });
 
 // manifesto page
 let manifesto_data = require('./views/data/manifesto.json')
 app.get('/manifesto', function(req, res) {
-  res.render('pages/manifesto', {manifesto_data: manifesto_data});
+  res.render('pages/manifesto', {manifesto_data: manifesto_data, branding_data: branding_data});
 });
 
 // solutions page
 let solutions_data = require('./views/data/solutions.json')
 app.get('/solutions', function(req, res) {
-    res.render('pages/solutions', {solutions_data: solutions_data});
+    res.render('pages/solutions', {solutions_data: solutions_data, branding_data: branding_data});
 });
 
 // contact page
 let contact_data = require('./views/data/contact.json')
 app.get('/contact', function(req, res) {
-    res.render('pages/contact', {contact_data: contact_data});
+    res.render('pages/contact', {contact_data: contact_data, branding_data: branding_data});
 });
 
 app.listen(8080);
