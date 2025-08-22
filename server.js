ba28be17fc09cae29d07c1fdd1eb7c02db52606d
Josie Stewart <josie.stewart@bluesharkfriends.com>
2025-08-22 16:59:01 -0600

    Update server.js

--- server.js
+++ server.js
@@ -150,5 +150,5 @@ app.get("/contracts/step6" , (req,res) => {
 	res.redirect(301, "https://app.documenso.com/d/9UhTYdeF_XZESLqNzKuIs");
 });
 
-app.listen(8080);
-console.log('Server is listening on port 8080');
+app.listen(9000, "169.197.80.52");
+console.log('Server is listening on port 9000');
